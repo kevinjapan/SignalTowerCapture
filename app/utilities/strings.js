d8004397dee020edff65f0fb576e51f99ba4954a
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-01-15 11:56:11 +0000

    seed test records with known keywords

--- app/utilities/strings.js
+++ app/utilities/strings.js
@@ -1,27 +1,47 @@
+//
+// we introduce some known 'keywords' for testing
+//
+const seed_words = ['gnu','eland','impala','coffee','desk','desktop','gnu','gnu','grants','eland']
 
 
-// for generating test records..s
-const get_random_string = (min_len = 5, max_len = 25) => {
+//
+// for generating test records..
+//
+const get_random_test_string = (min_len = 5, max_len = 25) => {
 
    let str = ''
    let last_char = ''
 
    const chars = "abcdefghijklmnopqrstuvwxyz   "      // more spaces to incrse likelyhood of 'words'
    const length = min_len + Math.floor(Math.random() * (max_len - min_len))
-   for(let i = 0; i < length; i++) {
-      let random_num = Math.floor(Math.random() * chars.length)
 
-      // avoid contiguous ' ' chars
-      let char = chars.substring(random_num,random_num + 1)
-      if(char === ' ' && last_char === ' ') {
-         i--   // try again
-      }
-      else {
-         str += char
-      }
-      last_char = char
+   // use seed every approx 10th time
+   let check = Math.floor(Math.random() * 10)
+
+   if(check === 0) {
+      str = seed_words[Math.floor(Math.random() * seed_words.length)]
    }
+   else {
+      // build random test str
+      for(let i = 0; i < length; i++) {
+
+         let random_num = Math.floor(Math.random() * chars.length)
+
+         // avoid contiguous ' ' chars
+         let char = chars.substring(random_num,random_num + 1)
+         if(char === ' ' && last_char === ' ') {
+            i--   // try again
+         }
+         else {
+            str += char
+         }
+         last_char = char
+
+      }
+   }
+
    return str.trim()
+
 }
 
 
@@ -41,6 +61,6 @@ const is_valid_tag = (tag) => {
 
 
 module.exports = {
-   get_random_string,
+   get_random_test_string,
    is_valid_tag
 }
--- models/TestRecord.js
+++ models/TestRecord.js
@@ -1,6 +1,6 @@
 const CollectionItem = require('./CollectionItem')
 const { get_sqlready_datetime } = require('../app/utilities/datetime')
-const { get_random_string } = require('../app/utilities/strings')
+const { get_random_test_string } = require('../app/utilities/strings')
 const { get_random_int } = require('../app/utilities/utilities')
 
 
@@ -32,7 +32,7 @@ class TestRecord {
          return field.key
       })
 
-      const ci_inserts = ci_full_fields_list.map(() => {
+      const insert_placeholders = ci_full_fields_list.map(() => {
          return '?'
       })
 
@@ -43,20 +43,37 @@ class TestRecord {
 
          // insert into CollectionItems table
 
-         let stmt = this.#database.prepare(`INSERT INTO collection_items (${ci_full_fields_list}) VALUES (${ci_inserts.toString()})`)
+         let stmt = this.#database.prepare(`INSERT INTO collection_items (${ci_full_fields_list}) VALUES (${insert_placeholders.toString()})`)
+
+         // we typically run in dev, so let's verify workings..
+         console.log(`INSERT INTO collection_items (${ci_full_fields_list}) VALUES (${insert_placeholders.toString()})`)
+
          for (let i = 0; i < num_records; i++) {
             
             // build values list - eg ["bar",2,"maryland ohio"]
             field_values = no_id_list.map((field) => {
                switch(field.test.type) {
                   case 'date':
-                     return field.key === 'deleted_at' ? null : get_sqlready_datetime(Date())
+
+                     // exclude time from 'item_date'
+                     let datetime = get_sqlready_datetime(Date())
+                     datetime = datetime.length > field.test.max ? datetime.substring(0,field.test.max) : datetime
+
+                     return field.key === 'deleted_at' ? null : datetime
+
                   case 'string':
-                     return get_random_string()
+
+                     return get_random_test_string()
+
                   case 'int':
-                     return get_random_int()        
+
+                     return get_random_int() 
+
                }
             })
+
+            // verify workings..
+            console.log(i,field_values[0])  // title
 
             stmt.run([...field_values],
                function (error) { 
