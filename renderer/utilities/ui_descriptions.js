9564799814a6b8384cff679145c6ba140782c25b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-01-24 12:31:22 +0000

    tidy descs for file_types

--- renderer/utilities/ui_descriptions.js
+++ renderer/utilities/ui_descriptions.js
@@ -2,10 +2,13 @@ const DESC = {
 
    DELETED_RECORDS: 
      `Deleted records are kept within the database for 31 days before being permanently deleted from the system.
-      You can recover a deleted record by searching through previously deleted records here.`,
-   
-   FILE_OR_FOLDER_ITEMS:
-      `To add multiple `
+      You can recover a deleted record by searching through previously deleted records here.`,   
+   FILE_ITEM_FILETYPE:
+      `Add a single page or file item. Use 'Find File' to select the file in your collections folders.`,
+   FOLDER_ITEM_FILETYPE:
+      `Add a multiple page or multiple file item - such as a photo collection - by referencing the folder 
+       containing your files here.
+       Use 'Find File' to choose one of the images inside that folder as an example to display on the record.`,
 
 }
 
