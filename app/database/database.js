da6af7260e27c1020f0e292452b0bf35cd22bdc1
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-01-26 11:44:32 +0000

    rationalized database.js code

--- app/database/database.js
+++ app/database/database.js
@@ -78,63 +78,21 @@ class Database {
    // create the database tables
    // sqlite PRIMARY KEY auto-increments by default (we do not explicitly use AUTOINCREMENT)
    //
-   // to do : tidy/rationalize this func  - separate func for add_columns ?
-   //
    create_tables = async() => {
 
       this.#db.serialize(async() => {
 
          // CollectionItems table
          //
-
-         const collection_items_full_fields = get_full_fields('collection_items')
-         //console.log('collection_items_full_fields',collection_items_full_fields)
-
-
          let ci_create_cols_csv = get_table_create_fields('collection_items')
-         // console.log('ci_create_cols_csv',ci_create_cols_csv)
-
          this.#db.run(`CREATE TABLE IF NOT EXISTS collection_items (${ci_create_cols_csv})`, function (error) {
                if(error) console.log('There was an error initializing the database. ',error.message)
             }
          )
 
-         // add any columns added to CollectionItem since current database initialization
-         // permits additions to integrate w/ existing records
-         // to do : ensure we only run if table was *not* created this time.
+         this.add_new_collection_item_columns()
 
-         // get current cols in database.'collection_items'
-         const result = await new Promise((resolve,reject) => {
-            this.#db.all(`SELECT GROUP_CONCAT(NAME,',') as cols FROM PRAGMA_TABLE_INFO('collection_items')`, function (error,rows) {
-                  if(error) console.log('There was an error reading table info from the database. ',error.message)
-                  resolve(rows[0])
-               }
-            )
-         }).catch((error) => {
-            this.set_last_error(error)
-         })
-
-         // find any differences w/ CollectionItem fields list
-         let cols_not_in_database = []
-         const current_fields = result.cols.split(',') 
-         const collection_items_insert_fields = get_table_insert_fields('collection_items')
-         cols_not_in_database = collection_items_insert_fields.filter(field => 
-            !current_fields.includes(field)
-         )
-
-         //sqlite has no IF NOT EXISTS for ADD COLUMN - so we handle exception if cols already exists
-         cols_not_in_database.forEach(col => {
-            try {
-               const data_type = collection_items_full_fields.filter(field => field.key === col)[0].data_type
-               this.#db.run(`ALTER TABLE collection_items ADD COLUMN ${col} ${data_type}`)
-            } 
-            catch (error) {
-               console.log(`Failed to ADD COLUMN ${col} ${data_type} to collection_items table`,error)
-            }
-         })
-
-
-         // CollectionItems FTS virtual table
+         // CollectionItems_fts table
          // we don't include 'data_type' for full text search virtual table
          //
          let collection_items_fts_fields = get_table_insert_fields('collection_items_fts')
@@ -160,9 +118,61 @@ class Database {
             }
          )
 
-         // Triggers for FTS
-         // synch 'collection_items_fts' w/ 'collection_items'
-         //   
+         this.create_fts_triggers()
+      })
+   }
+
+
+   //
+   // exectute Add Column for any new CollectionItem fields currently not in the 'collection_items' database table
+   // future : expand this to add cols to any of the tables
+   //
+   add_new_collection_item_columns = async() => {
+
+      const collection_items_full_fields = get_full_fields('collection_items')
+
+      // add any columns added to CollectionItem since current database initialization
+      // permits additions to integrate w/ existing records
+      // to do : ensure we only run if table was *not* created this time.
+
+      // get current cols in database.'collection_items'
+      const result = await new Promise((resolve,reject) => {
+         this.#db.all(`SELECT GROUP_CONCAT(NAME,',') as cols FROM PRAGMA_TABLE_INFO('collection_items')`, function (error,rows) {
+               if(error) console.log('There was an error reading table info from the database. ',error.message)
+               resolve(rows[0])
+            }
+         )
+      }).catch((error) => {
+         this.set_last_error(error)
+      })
+
+      // find any differences w/ CollectionItem fields list
+      let cols_not_in_database = []
+      const current_fields = result.cols.split(',') 
+      const collection_items_insert_fields = get_table_insert_fields('collection_items')
+      cols_not_in_database = collection_items_insert_fields.filter(field => 
+         !current_fields.includes(field)
+      )
+
+      //sqlite has no IF NOT EXISTS for ADD COLUMN - so we handle exception if cols already exists
+      cols_not_in_database.forEach(col => {
+         try {
+            const data_type = collection_items_full_fields.filter(field => field.key === col)[0].data_type
+            this.#db.run(`ALTER TABLE collection_items ADD COLUMN ${col} ${data_type}`)
+         } 
+         catch (error) {
+            console.log(`Failed to ADD COLUMN ${col} ${data_type} to collection_items table`,error)
+         }
+      })
+   }
+
+
+   //
+   // Triggers for FTS
+   // synch 'collection_items_fts' w/ 'collection_items'
+   //
+   create_fts_triggers() {
+
          let fts_insert_fields = get_table_insert_fields('collection_items_fts')
          let fts_insert_fields_values = fts_insert_fields.map(field => `NEW.${field}`)
 
@@ -205,10 +215,9 @@ class Database {
                if(error) console.log('There was an error initializing the database. ',error.message)
             }
          )
-
-         
-      })
    }
+
+
 
    close() {
       this.#db.close()
