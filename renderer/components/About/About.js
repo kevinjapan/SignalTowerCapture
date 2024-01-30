dae222895bd7f99e200b63860198e306ac9b25be
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-01-30 14:09:24 +0000

    update about page

--- renderer/components/About/About.js
+++ renderer/components/About/About.js
@@ -15,18 +15,35 @@ class About {
       let about_section = create_section()
 
       const heading = create_h({
-         level:'h3',
-         text:'Signal Tower Capture'
-      })
-      const sub_heading = create_h({
          level:'h1',
          text:'About'
       })
+      const section_heading = create_h({
+         level:'h2',
+         classlist:['logo_heading'],
+         text:'Signal Tower Capture'
+      })
+      const tagline = create_h({
+         level:'h3',
+         text:'Local Collections Management'
+      })
 
-      // to do : about text here..
+      const lead_text = create_p({
+         text:`Signal Tower Capture is a desktop collections solution for managing files and folders on your local machine and referencing these to your local on-site collection. It is ideally suited for smaller museums or archives where you need a simple solution with zero-overheads and simplicity of use.`
+      })
+
+      
+      const tech_heading = create_h({
+         level:'h3',
+         text:'Technologies'
+      })
+      const tech_text = create_p({
+         text:`Signal Tower Capture is built on the open-source Electron application framework and public domain SQLite database.`
+      })
+      
 
       // assemble
-      about_section.append(heading,sub_heading)
+      about_section.append(heading,section_heading,tagline,lead_text,tech_heading,tech_text)
       return about_section
    }
 
