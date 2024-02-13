ae438c80f671f8ead84c65393c24be52b3f34e4e
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-02-13 10:58:39 +0000

    Update Files.js

--- renderer/components/Files/Files.js
+++ renderer/components/Files/Files.js
@@ -37,13 +37,8 @@ class Files {
    }
 
    constructor(props) {
-      this.#props = props
-      console.log('Files.props',props)
-      
-      // to do : if props contains specified folder - open that folder immediately..
-
-      // this.#props.context.selected_folder  
-
+      if(props) this.#props = props
+      console.log('Files',props)
    }
 
 
@@ -67,54 +62,27 @@ class Files {
             {key:'id',value:'files_outcome'}
          ]
       })
-      const temp = create_div({
-         text:this.#props ? this.#props.context.selected_folder : 'no folder selected'
+      const folder_selected = create_div({
+         attributes:[
+            {key:'id',value:'folder_selected'},
+         ],
+         classlist:['p_1'],
+         text:this.#props ? this.#props.context.selected_folder : 'Please select a folder.'
       })
 
-
-      // to do : open 'selected_folder' if provided - put in sep func - possible lib. func
-
-      // if we have a provided 'selected_folder', we are coming 'back' from a Record, open the folder..
-      if(this.#props) {
-         if(this.#props.context) {
-            if(this.#props.context.selected_folder) {
-               
-               // to do : do we need to prepend root_folder here?
-               console.log('folder',this.#props.context.selected_folder)
-
-               const files_list = await window.files_api.getFolderFilesList(this.#props.context.selected_folder)
-
-               console.log('files_list',files_list)
-
-               // returns array of file objects:
-               // filename: "bellrock.jpg"
-               // path: "C:\\wamp64\\www\\dev\\signal-tower-capture\\collection-dataset\\Research_A_G\\bell-rock-lighthouse"
-               // type: "file"
-
-               // to do : now display to UI
-
-            }
-         }
-      }
-
-      files_section.append(heading,temp,files_outcome)
-
-      // get root_folder path
-      // we only permit files under the Collection Root Folder
-      // to do : verify file paths are in root folder
+      files_section.append(heading,files_outcome)
 
       let result = await window.config_api.getAppConfig()
       
-      if(typeof result != "undefined") {
-      
+      if(typeof result != "undefined") {      
          if(result.outcome === 'success') {
 
             this.#root_folder = result.app_config.root_folder
 
             // root folder
             const root_sub_heading = create_h({
-               level:'h4',
-               text:this.#root_folder
+               level:'h5',
+               text:'Root folder: ' + this.#root_folder
             })
 
             // open folder
@@ -146,12 +114,54 @@ class Files {
 
             files_layout.append(file_list,file_view)
 
+
             // assemble
             files_section.append(
                root_sub_heading,
                open_folder_btn,
+               folder_selected,
                files_layout
             )
+            
+
+            // if we have a provided 'selected_folder', we are coming 'back' from a Record, open the folder..
+            if(this.#props) {
+               if(this.#props.context) {
+                  if(this.#props.context.selected_folder) {                    
+
+                     const files_list = await window.files_api.getFolderFilesList(`${this.#root_folder}${this.#props.context.selected_folder}`)
+
+                     if(files_list) {
+                        let list = create_ul({classlist:['flex','flex_col','gap_0.5','m_0','p_0']})
+                        let list_item
+                        files_list.forEach(file => {
+
+                           // assign folder_path to context, removing the 'root_folder' part
+                           this.#context.field_filters[0].value = file.path.replace(this.#root_folder,'')
+                           console.log('this.#context.field_filters[0].value',this.#context.field_filters[0].value)
+      
+                           if(file.type === 'file') {
+                              list_item = create_li({
+                                 attributes:[
+                                    {key:'data-file-path',value:file.path + '\\' + file.filename},
+                                    {key:'data-file-name',value:file.filename}
+                                 ],
+                                 classlist:['folder_item','cursor_pointer','m_0','p_0'],
+                                 text:file.filename
+                              })
+                              list.append(list_item)
+                           }
+                        })
+                        // assemble
+                        file_list.replaceChildren(list)
+                        setTimeout(() => this.activate_file_links(),100)
+
+                        // get our reference matching records
+                        await this.get_matching_records()
+                     }
+                  }
+               }
+            }
          }
          else {
             setTimeout(() => Notification.notify('files_outcome','Sorry, we couldn\'t locate the Collection Root Folder'),200)
@@ -163,8 +173,11 @@ class Files {
       return files_section
    }
 
+ 
 
+   //
    // enable buttons/links displayed in the render
+   //
    activate = () => {
 
       // open folder
@@ -174,10 +187,12 @@ class Files {
          open_folder_btn.addEventListener('click',async(event) => {
 
             const files_list = await window.files_api.openFolderDlg(this.#root_folder)
+            const folder_selected = document.getElementById('folder_selected')
             
             if(files_list) {
 
                const file_list = document.getElementById('file_list')
+               const file_view = document.getElementById('file_view')
                let list = create_ul({classlist:['flex','flex_col','gap_0.5','m_0','p_0']})
                let list_item
 
@@ -189,6 +204,7 @@ class Files {
 
                      // assign folder_path to context remove the 'root_folder' part from path
                      this.#context.field_filters[0].value = file.path.replace(this.#root_folder,'')
+                     if(folder_selected) folder_selected.innerText = file.path.replace(this.#root_folder,'')
 
                      if(file.type === 'file') {
                         list_item = create_li({
@@ -204,27 +220,25 @@ class Files {
                   })
                   
                   // assemble
-                  file_list.replaceChildren(list)
+                  if(file_list) file_list.replaceChildren(list)
+                  if(file_view) file_view.replaceChildren()
                   setTimeout(() => this.activate_file_links(),100)
                   
-                  // we get list of 'folder_path' matching records w/ single db call
-                  // rather than call db to check for each file we will check against this list
-                  console.log('getting matching records',this.#context)
-                  const result = await window.collection_items_api.getItems(this.#context)
-                  this.#matching_records = result.collection_items
-                  this.#record_fields = result.collection_item_fields
+                  await this.get_matching_records()
                }
                else {
-                  // to do : notify not in the Collections folders (path doesn't include root_folder)
-                  console.log('this is not on the root_folder path..')
+                  Notification.notify(
+                     'files_outcome',
+                     `The folder you selected is not within the Collections Folders.`)
                }
             }
          })
       }
    }
 
-
+   //
    // enable buttons/links displayed in the render
+   //
    activate_file_links = () => {
       // select file item
       const file_links = document.querySelectorAll('.folder_item')
@@ -269,9 +283,18 @@ class Files {
       return this.#matching_records.find(item => item.file_type.toUpperCase() === 'FOLDER')
    }
 
+   // get list of 'folder_path' matching records (single db call)
+   get_matching_records = async() => {
+      const result = await window.collection_items_api.getItems(this.#context)
+      console.log('matching_records : context',this.#context)
+      this.#matching_records = result.collection_items
+      this.#record_fields = result.collection_item_fields
+   }
+
    get_record_fields = () => {
       return this.#record_fields
    }
+
 }
 
 
