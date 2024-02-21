71b0eeb297cdeaecfc766a79ca6ed0ec82a01fc1
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-02-21 10:34:23 +0000

    add add_to_queue utility func

--- renderer/utilities/ui_utilities.js
+++ renderer/utilities/ui_utilities.js
@@ -48,3 +48,28 @@ export const build_img_elem = async(id,file_path,alt_text = 'image',attributes =
    })
    return img
 }
+
+
+
+//
+// add item to a queue
+// where queue is an array of objects (items)
+// returns new array
+//
+
+// to do : register in app_config.recent_records
+// - get app_config.recent_records
+// - modify recent_record (manage size of list)
+// - update recent_records with
+// - avoid duplicates (use Set?)
+
+export const add_to_queue = (queue,max_len,item) => {
+
+   // remove oldest items
+   while(queue.length >= max_len) {
+      queue.pop()
+   }
+   // push new item
+   return [item,...queue]
+   
+}
