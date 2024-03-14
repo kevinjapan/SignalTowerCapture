c60a5a5c54abb9e6f8f76d82ce4ceefc1c3c9442
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-03-14 11:26:58 +0000

    add get root_folder

--- renderer/components/FileInjector/FileInjector.js
+++ renderer/components/FileInjector/FileInjector.js
@@ -1,7 +1,8 @@
-import { create_section,create_h, create_p,create_div } from '../../utilities/ui_elements.js'
 import CollectionItemCard from '../CollectionItemCard/CollectionItemCard.js'
 import CollectionItemInjectForm from '../CollectionItemInjectForm/CollectionItemInjectForm.js'
-
+import { init_card_img_loads } from '../../utilities/ui_utilities.js'
+import { trim_end_char } from '../../utilities/ui_strings.js'
+import { create_section,create_p,create_div } from '../../utilities/ui_elements.js'
 
 class FileInjector {
 
@@ -14,12 +15,14 @@ class FileInjector {
 
    render = async() => {
       
+      // get root_folder
+      const app_config_obj = await window.config_api.getAppConfig()
+      if(app_config_obj.outcome === 'success') {      
+         this.#props.root_folder = trim_end_char(app_config_obj.app_config.root_folder,'\\')            
+      }
+
       let file_injector = create_section()
 
-      const heading = create_h({
-         level:'h3',
-         text:'File Injector'
-      })
 
       const outcome_classes = ['bg_lightgrey','mt_1','pl_1','pr_1']
       const outcome_attrs = [{key:'id',value:'export_csv_outcome'}]
@@ -44,18 +47,16 @@ class FileInjector {
                   move that file out of this folder.`
          })
          // assemble
-         file_injector.append(heading,check_outcome)
-         
+         file_injector.append(check_outcome)
+
          const collection_item_card = new CollectionItemCard(this.#props)
          file_injector.appendChild(collection_item_card.render(record_fields,matching_folder_record))
-
-         console.log('props going',this.#props)
 
          setTimeout(() => collection_item_card.activate(),200)
 
       }
       else if(matching_file_record) {
-         
+
          // a matching record for this file was found
          const check_outcome = create_div({
             classlist:outcome_classes,
@@ -64,17 +65,17 @@ class FileInjector {
          })
          
          // assemble
-         file_injector.append(heading,check_outcome)
+         file_injector.append(check_outcome)
+
          const collection_item_card = new CollectionItemCard(this.#props)
          file_injector.appendChild(collection_item_card.render(record_fields,matching_file_record))
 
          setTimeout(() => collection_item_card.activate(),200)
          
       }
-      else {
-         
-         // no matching record was found for this file
-         
+      else {         
+
+         // no matching record was found for this file         
          const check_outcome = create_div({
             classlist:outcome_classes,
             attributes:outcome_attrs,
@@ -85,18 +86,19 @@ class FileInjector {
          })
    
          // assemble
-         file_injector.append(heading,check_outcome,file)
+         file_injector.append(check_outcome,file)
          const inject_form = new CollectionItemInjectForm(this.#props)
          file_injector.appendChild(await inject_form.render())
 
-      }    
+      }
+
       return file_injector
    }
 
 
    // enable buttons/links displayed in the render
    activate = () => {
-
+      init_card_img_loads()
    }
 
 }
