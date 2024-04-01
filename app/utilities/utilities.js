da228f65db989f4b7af8b8f7f3f9c969c3e327b2
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-04-01 11:30:10 +0100

    add assoc_arr_obj()

--- app/utilities/utilities.js
+++ app/utilities/utilities.js
@@ -5,9 +5,22 @@ const get_random_int = (min_value = 5, max_value = 15) => {
    return value
 }
 
+// 
+// Create assoc arr as obj from two 1-d arrays
+//
+const assoc_arr_obj = (arr_1,arr_2) => {
+
+   let assoc_arr_obj = {}
+   for(let i = 0; i < arr_1.length; i++) {
+      assoc_arr_obj[arr_1[i]] = arr_2[i]
+   }
+   return assoc_arr_obj
+}
+
 
 
 
 module.exports = {
    get_random_int,
+   assoc_arr_obj
 }
