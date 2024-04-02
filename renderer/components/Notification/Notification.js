45e80d528356872cd440924d3a63f4c4847a4d8a
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-04-02 18:40:49 +0100

    handle message_arr for separate <p> elements

--- renderer/components/Notification/Notification.js
+++ renderer/components/Notification/Notification.js
@@ -1,12 +1,14 @@
-import { create_div } from '../../utilities/ui_elements.js'
+import { create_div,create_p } from '../../utilities/ui_elements.js'
 
 
 
 class Notification {
 
-   static notify = (elem_selector,message,classes = [],fade_out = true) => {
+   static notify = (elem_selector,message_arr,classes = [],fade_out = true) => {
 
-      if(message === '' ) return
+      if(message_arr === undefined || message_arr === '' ) return
+      if(typeof message_arr === 'string') message_arr = [message_arr]
+      if(!Array.isArray(message_arr)) return
 
       // default is warning
       if(!Array.isArray(classes) || classes.length === 0) classes = ['bg_yellow_300']
@@ -14,11 +16,15 @@ class Notification {
       const elem_selectors = document.querySelectorAll(elem_selector)
       if(elem_selectors) {
          elem_selectors.forEach(elem => {
+            elem.replaceChildren()
             const notification_card = create_div({
-               classlist:['notification',...classes,'p_1'],
-               text:message
+               classlist:['notification',...classes,'p_1']
             })
-            elem.replaceChildren(notification_card) 
+            message_arr.forEach(msg => {
+               notification_card.append(create_p({text:msg}))
+            })
+
+            elem.append(notification_card) 
             if(fade_out) setTimeout(() => this.remove_notification(elem_selector),7000)
          })
       }
@@ -35,7 +41,6 @@ class Notification {
    }
 
 }
-
 
 
 export default Notification
