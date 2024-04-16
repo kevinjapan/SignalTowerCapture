c539dbd0c336a7b41451c9480f554daa00e2a19f
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-04-16 12:10:57 +0100

    fix notifications on import json file

--- models/ImportCSVFile.js
+++ models/ImportCSVFile.js
@@ -25,90 +25,92 @@ class ImportCSVFile {
       const fs = require('fs')
       const readline = require('node:readline')
 
-         if(fs.existsSync(file_path)) {
-            const read_stream = fs.createReadStream(file_path)
-            const rl = readline.createInterface({
-               input:read_stream,
-               crlfDelay:Infinity,
-            })
-            
-            const collection_item = new CollectionItem(this.#database)
+      if(fs.existsSync(file_path)) {
 
-            let count = 0
+         const read_stream = fs.createReadStream(file_path)
+         const rl = readline.createInterface({
+            input:read_stream,
+            crlfDelay:Infinity,
+         })
+         
+         const collection_item = new CollectionItem(this.#database)
 
-            // we check first line and only proceed if it matches expected fields and data validation
-            let first_line = true
+         let count = 0
 
-            for await (const line of rl) {
+         // we check first line and only proceed if it matches expected fields and data validation
+         let first_line = true
 
-               // we test first line for correct no. fields and that fields are valid (assuming all lines same format!)
-               if(first_line) {
+         for await (const line of rl) {
 
-                  const valid_first_line = is_valid_collection_item_csv(CollectionItem.get_full_fields_list(),line)
+            // we test first line for correct no. fields and that fields are valid (assuming all lines same format!)
+            if(first_line) {
+               
+               const valid_first_line = is_valid_collection_item_csv(CollectionItem.get_full_fields_list(),line)
 
-                  // bail and notify if import file does not match expected fields
-                  if(valid_first_line.outcome === 'fail') {
-                     const error_strs = valid_first_line.errors.map(error => {
-                        return `${error.name} : ${error.message} : ${error.value}`
-                     })
-                     return  {
-                        query:'import_csv_file',
-                        outcome:'fail',
-                        message_arr:[
-                           'The file does not contain valid records.',
-                           '0 records were created.',
-                           ...error_strs
-                        ]
-                     }
-                  }
-               }
+               // bail and notify if import file does not match expected fields
+               if(valid_first_line.outcome === 'fail') {
 
-               // prevent further line/record validation (we assume format followed throughout file)
-               first_line = false
-
-               let promise_result = await new Promise(async(resolve,reject) => {
-      
-                  let result = await collection_item.create_from_csv(line)
-                  if(result.outcome === 'success') {   
-                     count++           
-                     resolve(result)
-                  }
-                  else {
-                     reject(result.message)
-                  }
-               }).catch((error) => {
-                  this.set_last_error(error)
-               })
-
-               if(promise_result) {
-                  // do nothing
-               }
-               else {
-                  let fail_response = {
+                  const error_strs = valid_first_line.errors.map(error => {
+                     return `${error.name ? error.name + ':' : ''} ${error.message} ${error.value ? error.value + ':' : ''}`
+                  })
+                  return  {
                      query:'import_csv_file',
                      outcome:'fail',
                      message_arr:[
-                        count + ' records were created. ' ,
-                        this.#last_error ,
-                        'No more records were processed after line ' + count + '.'
+                        'The file does not contain valid records.',
+                        '0 records were created.',
+                        ...error_strs
                      ]
                   }
-                  this.clear_last_error()
-                  return fail_response
                }
             }
-         }
-         else {
-            return  {
-               query:'import_csv_file',
-               outcome:'fail',
-               message:'The file was not found.' + file_path
+
+            // prevent further line/record validation (we assume format followed throughout file)
+            first_line = false
+
+            let promise_result = await new Promise(async(resolve,reject) => {
+   
+               let result = await collection_item.create_from_csv(line)
+               if(result.outcome === 'success') {   
+                  count++           
+                  resolve(result)
+               }
+               else {
+                  reject(result.message)
+               }
+            }).catch((error) => {
+               this.set_last_error(error)
+            })
+
+            if(promise_result) {
+               // do nothing
+            }
+            else {
+               let fail_response = {
+                  query:'import_csv_file',
+                  outcome:'fail',
+                  message_arr:[
+                     count + ' records were created. ' ,
+                     this.#last_error ,
+                     'No more records were processed after line ' + count + '.'
+                  ]
+               }
+               this.clear_last_error()
+               return fail_response
             }
          }
-         return {
+      }
+      else {
+         return  {
             query:'import_csv_file',
-            outcome:'success'
+            outcome:'fail',
+            message:'The file was not found.' + file_path
          }
+      }
+      return {
+         query:'import_csv_file',
+         outcome:'success'
+      }
    }
 
    //
