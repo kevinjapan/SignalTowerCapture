cbbd1b3eac8fb9ac37edccecbda5a695cf48e2a9
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-05-15 13:11:41 +0100

    Create Input.js

---
+++ renderer/components/Forms/Input/Input.js
@@ -0,0 +1,27 @@
+import { create_input } from '../../../utilities/ui_elements.js'
+
+
+class Input {
+
+   static render = (field,curr_field_value) => {      
+   
+      const field_input = create_input({
+         attributes:[
+            {key:'id',value:field.key},
+            {key:'name',value:field.key},
+            {key:'type',value:'text'},
+            {key:'value',value:curr_field_value},
+            {key:'maxlength',value:field.test.max},
+            {key:'readonly',value:field.readonly ? 'readonly' : false},
+         ],
+         classlist:['input_field']
+      })
+      if(field.hidden === true) field_input.hidden = true
+      if(field.placeholder) field_input.setAttribute('placeholder',field.placeholder)
+      if(!field.editable) field_input.disabled = 'disabled'
+
+      return field_input
+   }
+}
+
+export default Input
