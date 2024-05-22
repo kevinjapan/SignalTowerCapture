4cae6c13333e327afce4ab1d331fea61e16a6a78
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-05-22 13:15:04 +0100

    Update utilities.js

--- app/utilities/utilities.js
+++ app/utilities/utilities.js
@@ -10,7 +10,6 @@ const get_random_int = (min_value = 5, max_value = 15) => {
 // Create assoc arr as obj from two 1-d arrays
 //
 const assoc_arr_obj = (arr_1,arr_2) => {
-
    let assoc_arr_obj = {}
    for(let i = 0; i < arr_1.length; i++) {
       assoc_arr_obj[arr_1[i]] = arr_2[i]
