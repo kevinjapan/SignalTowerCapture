4a523ffb689e968446283fbce0d0f4c14337481b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-05-22 14:46:42 +0100

    refactor Import models

--- models/ImportCSVFile.js
+++ models/ImportCSVFile.js
@@ -1,8 +1,7 @@
 const CollectionItem = require('./CollectionItem')
 const { is_valid_collection_item_csv } = require('../app/utilities/validation')
-const { assoc_arr_obj } = require('../app/utilities/utilities')
-const { chunk_array } = require('../app/utilities/utilities')
-const { read } = require('fs')
+const { assoc_arr_obj,chunk_array } = require('../app/utilities/utilities')
+
 
 
 
@@ -16,7 +15,7 @@ class ImportCSVFile {
    #last_error
 
    // sql - the maximum number of rows in one VALUES clause is 1000
-   #batch_size = 10 // to do : test w/ 100
+   #batch_size = 10 // to do : test w/ 100 - rollout to ImportJSONFile
 
    constructor(database) {
       this.#database = database
@@ -36,17 +35,9 @@ class ImportCSVFile {
                input:read_stream,
                crlfDelay:Infinity,
             })
-            
-            const collection_item = new CollectionItem(this.#database)
-
-            let count = 0
-
+  
             // we check first line and only proceed if it matches expected fields and data validation
             let first_line = true
-
-            //
-            // to do : we have to refactor as we did w/ ImportJSONFile
-            //
 
             // we will build arr of collection_items from lines
             let collection_items = []
@@ -54,7 +45,6 @@ class ImportCSVFile {
             let values         
             for await (const line of rl) {
 
-               // to do : package/farm this & re-use in ImportJSONFile?
                // we test first line for correct no. fields and that fields are valid (assuming all lines same format!)
                if(first_line) {                  
                   const valid_first_line = is_valid_collection_item_csv(CollectionItem.get_full_fields_list(),line)
@@ -83,22 +73,50 @@ class ImportCSVFile {
                values = line.split(',')
                collection_items.push(assoc_arr_obj(field_keys,values))
             }
-            const num_rcvd_items = collection_items.length
 
-            const no_duplicate_collection_items = await this.remove_duplicates(collection_items)
+            const num_rcvd_items = collection_items.length
+            const no_duplicate_collection_items = await this.remove_duplicate_records(collection_items)
             const num_non_duplicate_items = no_duplicate_collection_items.length
 
-            // log action (to do : log where?)
-            console.log(num_non_duplicate_items,'records were added from the',num_rcvd_items,'read.',num_rcvd_items - num_non_duplicate_items,'records were duplicates.')
-
+            if(num_non_duplicate_items < 1) {
+               return {
+                  query:'import_csv_file',
+                  outcome:'success',
+                  message_arr:[
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} lines read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+            }
+               
             const result = await this.process_records(no_duplicate_collection_items)
-
-            // ----------------------------------------------------------------------
-            // to do : enable wait dlg as w/ ImportJSONFile
-            // ----------------------------------------------------------------------
-
-            return {outcome:'success'} // to do : temp to verify UI front-end closes dlg
-            //         we need actual response ('result' above) from main process
+         
+            if(result) {
+               return {
+                  query:'import_csv_file',
+                  outcome:'success',
+                  last_id:last_id,
+                  changes:changes,
+                  message_arr:[
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} lines read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+            }
+            else {
+               let fail_response = {
+                  query:'import_csv_file',
+                  outcome:'fail',
+                  message_arr:[
+                     `There was an error attempting to import the CSV file. [ImportCSVFile.import]`,
+                     // this.#last_error.message,
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} lines read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+               this.clear_last_error()
+               return fail_response
+            }
          }
          else {
             throw 'The import file was not found.'
@@ -112,96 +130,39 @@ class ImportCSVFile {
             message:'There was an error attempting to import the records. [ImportCSVFile.import]  ' + error
          }
       }
-      // }
-      // else {
-      //    return  {
-      //       query:'import_csv_file',
-      //       outcome:'fail',
-      //       message:'The file was not found.' + file_path
-      //    }
-      // }
-      // return {
-      //    query:'import_csv_file',
-      //    outcome:'success'
-      // }
    }
 
    //
    // Duplicate check
-   // Remove any items for which we already have a record
-   // the unique key is 'folder_path\\file_name'
    //
-   // to do : duplicates code in ImportJSONFile.js - farm out?
-   //
-   remove_duplicates = async(collection_items) => {
-
-      const filtered_items = []
-
-      for(const item of collection_items) {         
-         const collection_item_obj = new CollectionItem(this.#database)
-         let attempt_read_existing = await collection_item_obj.read({
-            page:1,
-            field_filters:[
-               {field:'folder_path',value:item.folder_path},
-               {field:'file_name',value:item.file_name}
-            ]
-         })
-         if(attempt_read_existing.outcome === 'success') {
-            if(attempt_read_existing.collection_items.length === 0) {
-               // ok to proceed - we will create a record for this item
-               filtered_items.push(item)
-            }
-         }
-      }
-      return filtered_items
+   remove_duplicate_records = async(collection_items) => {      
+      const collection_item_obj = new CollectionItem(this.#database)
+      return await collection_item_obj.filter_out_duplicates(collection_items)
    }
 
-     //
+
+   //
    // Batch insert records (max permitted is 1000 values)
    // We assume all are duplicate_checked prior to submission here
+   // CollectionItem.create_batch will exclude 'id'
    //
    process_records = async(collection_items) => {
 
-      // to do : remove 'id' ?
-
-
       if(!Array.isArray(collection_items) || collection_items.length < 1) return false
 
-      const collection_item = new CollectionItem(this.#database)
-      
+      const collection_item = new CollectionItem(this.#database)      
       let promise_result = null
       const arr_of_chunks = chunk_array(collection_items,this.#batch_size)
  
       promise_result = await new Promise(async(resolve,reject) => {
-
          // submit each 'chunk' of items for batch INSERT
          for(const chunk of arr_of_chunks) {
-            let result = await collection_item.create_batch(chunk)    
-            if(result.outcome === 'success') {
-               resolve({outcome:'success'})
-            }
-            else {
-               reject({outcome:'fail'})
-            }
+            await collection_item.create_batch(chunk)
          }
          resolve({outcome:'success'})
       })
 
-      console.log('RESULT',result)
-     
-      // to do : adapt this handling block
-      if(promise_result) {
-         // nothing
-      }
-      else {
-         let fail_response = {
-            query:'import_json_file',
-            outcome:'fail',
-            message:'There was an error attempting to create the record. [ImportJSONFile.process_records]  ' + this.#last_error
-         }
-         this.clear_last_error()
-         return fail_response
-      }
+      return promise_result
    }
 
    //
--- models/ImportJSONFile.js
+++ models/ImportJSONFile.js
@@ -2,7 +2,9 @@ const CollectionItem = require('./CollectionItem')
 const { chunk_array } = require('../app/utilities/utilities')
 
 
+
 // Import a JSON file of CollectionItem records.
+
 
 class ImportJSONFile {
 
@@ -11,7 +13,7 @@ class ImportJSONFile {
    #last_error
 
    // sql - the maximum number of rows in one VALUES clause is 1000
-   #batch_size = 10 // to do : test w/ 100
+   #batch_size = 10
 
 
    constructor(database) {
@@ -25,10 +27,49 @@ class ImportJSONFile {
       try {
          if(fs.existsSync(file_path)) {
             const collection_items = await this.get_json_file_contents(file_path)
-            const no_duplicate_collection_items = await this.remove_duplicates(collection_items)
+            const num_rcvd_items = collection_items.length
+            const no_duplicate_collection_items = await this.remove_duplicate_records(collection_items)
+            const num_non_duplicate_items = no_duplicate_collection_items.length
+            
+            if(num_non_duplicate_items < 1) {
+               return {
+                  query:'import_json_file',
+                  outcome:'success',
+                  message_arr:[
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} records read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+            }
+                        
             const result = await this.process_records(no_duplicate_collection_items)
-            return {outcome:'success'} // to do : temp to verify UI front-end closes dlg
-                                       //         we need actual response ('result' above) from main process
+
+            if(result) {
+               return {
+                  query:'import_csv_file',
+                  outcome:'success',
+                  last_id:last_id,
+                  changes:changes,
+                  message_arr:[
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} records read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+            }
+            else {
+               let fail_response = {
+                  query:'import_csv_file',
+                  outcome:'fail',
+                  message_arr:[
+                     `There was an error attempting to import the CSV file. [ImportCSVFile.import]`,
+                     // this.#last_error.message,
+                     `${num_non_duplicate_items} records were added from the ${num_rcvd_items} lines read.`,
+                     `${num_rcvd_items - num_non_duplicate_items} records were duplicates.`
+                  ]
+               }
+               this.clear_last_error()
+               return fail_response
+            }
          }
          else {
             throw 'The import file was not found.'
@@ -44,78 +85,37 @@ class ImportJSONFile {
       }
    }
 
-   // to do : we may have to manage json file sizes on export? - so export to batch files - not single monolithic file
-
    //
    // Duplicate check
-   // Remove any items for which we already have a record
-   // the unique key is 'folder_path\\file_name'
    //
-   remove_duplicates = async(collection_items) => {
-
-      const filtered_items = []
-
-      for(const item of collection_items) {         
-         const collection_item_obj = new CollectionItem(this.#database)
-         let attempt_read_existing = await collection_item_obj.read({
-            page:1,
-            field_filters:[
-               {field:'folder_path',value:item.folder_path},
-               {field:'file_name',value:item.file_name}
-            ]
-         })
-         if(attempt_read_existing.outcome === 'success') {
-            if(attempt_read_existing.collection_items.length === 0) {
-               // ok to proceed - we will create a record for this item
-               filtered_items.push(item)
-            }
-         }
-      }
-      return filtered_items
+   remove_duplicate_records = async(collection_items) => {      
+      const collection_item_obj = new CollectionItem(this.#database)
+      return await collection_item_obj.filter_out_duplicates(collection_items)
    }
 
 
    //
    // Batch insert records (max permitted is 1000 values)
    // We assume all are duplicate_checked prior to submission here
+   // CollectionItem.create_batch will exclude 'id'
    //
    process_records = async(collection_items) => {
 
       if(!Array.isArray(collection_items) || collection_items.length < 1) return false
 
-      const collection_item = new CollectionItem(this.#database)
-      
+      const collection_item = new CollectionItem(this.#database)      
       let promise_result = null
       const arr_of_chunks = chunk_array(collection_items,this.#batch_size)
- 
-      promise_result = await new Promise(async(resolve,reject) => {
 
+      promise_result = await new Promise(async(resolve,reject) => {
          // submit each 'chunk' of items for batch INSERT
          for(const chunk of arr_of_chunks) {
-            let result = await collection_item.create_batch(chunk)    
-            if(result.outcome === 'success') {
-               resolve({outcome:'success'})
-            }
-            else {
-               reject({outcome:'fail'})
-            }
+            await collection_item.create_batch(chunk)
          }
          resolve({outcome:'success'})
       })
-     
-      // to do : adapt this handling block
-      if(promise_result) {
-         // nothing
-      }
-      else {
-         let fail_response = {
-            query:'import_json_file',
-            outcome:'fail',
-            message:'There was an error attempting to create the record. [ImportJSONFile.process_records]  ' + this.#last_error
-         }
-         this.clear_last_error()
-         return fail_response
-      }
+
+      return promise_result
    }
 
 
