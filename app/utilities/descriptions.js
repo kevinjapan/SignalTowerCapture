197207abf687d5c296e06a52f7ebf1f5878ef3f4
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-06-13 12:43:28 +0100

    refactor / add Desc

--- app/utilities/descriptions.js
+++ app/utilities/descriptions.js
@@ -1,22 +1,17 @@
 const DESC = {
 
-   ROOT_FOLDER: {
-      body:`
-      This is the root folder for your Collection. We use this root folder to locate files within your Collection.`
-   },
-
-   EXPORT_FOLDER: {
-      body:'Export CSV files will be saved in this folder.'
-   },
-
-   ITEM_FIELDS: {
-      body:'The complete list of fields in a Collection Item Record.'
-   },
-
-   BACKUP_FOLDER: {
-      body:'Database backup files will be saved in this folder.'
-   }
-
+   ROOT_FOLDER:
+      `This is the root folder for your Collection. We use this root folder to locate files within your Collection.`,
+   EXPORT_FOLDER: 
+      'Export CSV files will be saved in this folder.',
+   ITEM_FIELDS: 
+      'The complete list of fields in a Collection Item Record.',
+   BACKUP_FOLDER: 
+      'Database backup files will be saved in this folder.',
+   EXCLUDED_SUB_FOLDERS: 
+      `Exclude sub-folders of the root_folder from the Collection Data Set. 
+       Files in these folders will be ignored by the system.
+       Enter a list of comma-separated values for names of the folders you wish to exclude from the system.`
 }
 
 module.exports = {
