c6bfdd981dd7d2d028909cf6f4f591ad60d2f5ed
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-06-14 16:31:42 +0100

    add days_between

--- app/utilities/datetime.js
+++ app/utilities/datetime.js
@@ -24,8 +24,19 @@ const get_sqlready_date_from_js_date = (date_obj) => {
    return ''
 }
 
+//
+// Calcs days btwn two dates - assumes 'today' for 2nd param if not given
+//
+const days_between = (start,end = new Date()) => {
+   const date_1 = new Date(start)
+   const date_2 = new Date(end)
+   const ms_diff = date_2.getTime() - date_1.getTime()
+   return Math.round(ms_diff / (1000 * 60 * 60 * 24)) 
+}
+
 
 module.exports = {
    get_sqlready_datetime,
-   get_sqlready_date_from_js_date
+   get_sqlready_date_from_js_date,
+   days_between
 }
