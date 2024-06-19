f86a44173659b58a8fea7a7f12adfa20396b247a
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-06-19 15:02:25 +0100

    fix disabled up arrow

--- renderer/components/ParentLink/ParentLink.js
+++ renderer/components/ParentLink/ParentLink.js
@@ -6,14 +6,15 @@ import { icon } from '../../utilities/ui_utilities.js'
 class ParentLink {
 
    render = (root_folder,parent_folder_path,enabled = true) => {
+
       return icon('arrow_up',
          [
-            {key:'id',value:'parent_link_elem'},
+            {key:'id',value:`${enabled ? 'parent_link_elem' : ''}`},
             {key:'data-parent-folder',value:parent_folder_path.replace(root_folder,'')},
             {key:'width',value:'14px'},
             {key:'height',value:'14px'}
          ],
-         ['cursor_pointer','mt_0','mb_1','ml_0',`${enabled ? '' : 'disabled_arrow_icon'}`]
+         [`${enabled ? 'cursor_pointer' : ''}`,'mt_0','mb_1','ml_0',`${enabled ? '' : 'disabled_arrow_icon'}`]
       )
    }
 
